75197cd9b88d3b161f02da1bdc97df750bca3ba7
kosta-17 <kosta-17@kosta-17-PC>
2014-03-25 16:46:54 +0900

    login

---
+++ NamooClubThreeWeb/src/com/namoo/ns1/web/user/LoginController.java
@@ -0,0 +1,52 @@
+package com.namoo.ns1.web.user;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.namoo.ns1.service.facade.TownerService;
+import com.namoo.ns1.service.factory.NamooClubServiceFactory;
+
+@WebServlet("/login.do")
+public class LoginController extends HttpServlet{
+
+	private static final long serialVersionUID = -8502310861719199625L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		HttpSession session = req.getSession();
+		TownerService townerservice = NamooClubServiceFactory.getInstance().getTownerService();
+		
+		String userID = req.getParameter("userID");
+		String userPS = req.getParameter("userPS");
+		
+		//로그인 된 경우
+		if(townerservice.loginAsTowner(userID, userPS)){
+			session.setAttribute("loginID", userID);
+			resp.sendRedirect("cmlist.xhtml");
+			return;
+		}
+		
+		//로그인 되지 않은 경우		
+		else{
+			resp.sendRedirect("main.xhtml");
+			return;
+		}
+	}
+
+	
+}
